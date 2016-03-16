5f125d8c39c51be530656574c4a21f3f932c5230
TianYang <yangbing0115@163.com>
2016-03-16 19:00:26 +0800

    TianYang

---
+++ TYPhotosAlbum/TYPhotosAlbum/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TYPhotosAlbum
+//
+//  Created by TianYang on 16/3/16.
+//  Copyright © 2016年 Tianyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TYPhotosAlbum/TYPhotosAlbum/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TYPhotosAlbum
+//
+//  Created by TianYang on 16/3/16.
+//  Copyright © 2016年 Tianyang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
